2e1a198ca00b5445be989703bb11567c04f17797
Dhroov Sankla <dhroovsankla2004@gmail.com>
2026-07-04 20:47:02 +0530

    chore: bootstrap project framework using spring initializr template

---
+++ developer-intelligence-agent/src/main/java/developer_intelligence_agent/DeveloperIntelligenceAgentApplication.java
@@ -0,0 +1,13 @@
+package developer_intelligence_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperIntelligenceAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperIntelligenceAgentApplication.class, args);
+	}
+
+}
