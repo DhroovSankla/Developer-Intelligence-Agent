29702ec8779cd28aa58e00504f2f0f94cef2bff3
Dhroov Sankla <dhroovsankla2004@gmail.com>
2026-07-08 11:20:02 +0530

    config: Add lombok and persistence JPA layer to create tables

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.postgresql:postgresql")
 
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
